8a91aa325a8bbbb061ac3de08df92df6d7cc1b69
Demonic <134345277+IKomangYoga@users.noreply.github.com>
2024-07-08 11:10:11 +0800

    update login

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -1,39 +1,62 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Untree.co">
+  <link rel="shortcut icon" href="favicon.png">
 
-  <title>PKL Finder</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+  <meta name="description" content="" />
+  <meta name="keywords" content="bootstrap, bootstrap4" />
 
-  <!-- Favicons -->
-  <link href="<?= base_url() ?>gambar/logomagangputih.png" rel="icon">
-  <link href="<?= base_url() ?>gambar/logomagangputih.png" rel="apple-touch-icon">
+		<!-- Bootstrap CSS -->
+		<link href="<?=base_url()?>assets/asset/css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+		<link href="<?=base_url()?>assets/asset/css/tiny-slider.css" rel="stylesheet">
+		<link href="<?=base_url()?>assets/asset/css/style.css" rel="stylesheet">
+		<title>Bread Factory</title>
+	</head>
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	<body>
 
-  <!-- Vendor CSS Files -->
-  <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/simple-datatables/style.css" rel="stylesheet">
+		<!-- Start Header/Navigation -->
+		<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
-  <!-- Template Main CSS File -->
-  <link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
+			<div class="container">
+				<a class="navbar-brand" href="index.html">Bread Factory<span>.</span></a>
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Updated: Nov 17 2023 with Bootstrap v5.3.2
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarsFurni">
+					<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+						<li class="nav-item active">
+							<a class="nav-link" href="<?php echo base_url('cmhs/dashboard'); ?>">Home</a>
+						</li>
+						<li><a class="nav-link" href="<?php echo base_url('cmhs/product'); ?>">Product</a></li>
+						<li><a class="nav-link" href="<?php echo base_url('cmhs/pesanan'); ?>">Pesananmu</a></li>
+						<li><a class="nav-link" href="about.html">About us</a></li>
+						<li><a class="nav-link" href="services.html">Services</a></li>
+						<li><a class="nav-link" href="contact.html">Contact us</a></li>
+					</ul>
+
+					<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+						<li><a class="nav-link" href="<?php echo base_url('cmhs/profile'); ?>"><img src="<?=base_url()?>assets/asset/images/user.svg"></a></li>
+						<li><a class="nav-link" href="<?php echo base_url('cmhs/keranjang'); ?>"><img src="<?=base_url()?>assets/asset/images/cart.svg"></a></li>
+					</ul>
+				</div>
+			</div>
+				
+		</nav>
+		<!-- End Header/Navigation -->
+
+
+
+
+		<script src="<?=base_url()?>assets/asset/js/bootstrap.bundle.min.js"></script>
+		<script src="<?=base_url()?>assets/asset/js/tiny-slider.js"></script>
+		<script src="<?=base_url()?>assets/asset/js/custom.js"></script>
+	</body>
+
+</html>    
